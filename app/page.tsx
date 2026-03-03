97b37fed4eb0d50b0ebbe277db35d5c76e8ef398
Firash Poyilthodi Chathamparambil <firash@eliteproeventsksa.com>
2026-03-04 01:30:36 +0300

    Create welcome page in app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to ElitePro!</h1>
+            <p>This is your new app page.</p>
+        </div>
+    );
+};
+
+export default WelcomePage;
